2b34c4516081ed989a2449a0cba63ab3f5fe88a0
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-03-17 12:30:40 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PIXEL_ID = "783027081098083";
+const ACCESS_TOKEN = "EAFYzF3XBTyUBQ2trQKtZBuKG8sqo6IxXCzeRSZCkVAT8h8hVXRViTC4ONdZAsYyc0zacR7hU1uguSClQZBJoZBqNWpg71J53il1AFZBFwZB40Nhvq01CfpRSVsihwykJ0rBYc5QWWGKWajTNw5bEOvhj5vFqb3OhdxCDTiaATetZBTMjfeNAQ9CAnmnhFZCJmE4orDwZDZD";
+const API_VERSION = "v21.0"; // use your current Meta Graph API version
